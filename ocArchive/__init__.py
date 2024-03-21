b1140b08f39fdfa674c80dc3c543c6c35c177359
cosmicCode42 <cosmicanswer@gmail.com>
2024-03-21 18:15:24 +0000

    coded models.py, renamed module to ocArchive

---
+++ ocArchive/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+
+db = SQLAlchemy(app)
+
+
+from ocArchive import routes
---
+++ ocArchive/models.py
@@ -0,0 +1,36 @@
+from ocArchive import db
+
+
+class User(db.Model):
+    # schema for the User model
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50), unique=True, nullable=False)
+    user_password = db.Column(db.VarChar(15), unique=True, nullable=False)
+    characters = db.relationship("Character", backref="user", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        return self.user_name
+
+
+class Genre(db.Model):
+    #schema for the Genre model
+    id = db.Column(db.Integer, primary_key=True)
+    genre_name = db.Column(db.String(100), unique=True, nullable=False)
+    characters = db.relationship("Character", backref="genre", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        return self.genre_name
+
+
+class Character(db.Model):
+    # schema for the Character model
+    id = db.Column(db.Integer, primary_key=True)
+    char_name = db.Column(db.String(125), unique=True, nullable=False)
+    char_blurb = db.Column(db.String(250), unique=True, nullable=False)
+    char_descript = db.Column(db.String(5000), unique=True)
+    char_is_usable = db.Column(db.Boolean)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id", ondelete="CASCADE"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        return self.char_name
