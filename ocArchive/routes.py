2441e190a7ecf23d98962c2ce0edcb4c6625fde4
cosmicCode42 <cosmicanswer@gmail.com>
2024-03-26 21:49:15 +0000

    added id register route

--- ocArchive/routes.py
+++ ocArchive/routes.py
@@ -52,3 +52,23 @@ def characters():
     genres = list(Genre.query.order_by(Genre.genre_name).all())
     chars = list(Character.query.order_by(Character.char_name).all()) # puts all characters in a list
     return render_template("characters.html", chars=chars, genres=genres)
+
+
+@app.route("/id_gain", methods=["GET", "POST"])
+def id_gain():
+    # page to create a username and password to use in handling characters
+    users = list(User.query.order_by(User.user_name).all())
+    if request.method == "POST":
+        user = User(
+            user_name=request.form.get("user_name"),
+            user_password=request.form.get("user_password")
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("id_gain.html", users=users)
+
+
+    # user_name = db.Column(db.String(50), unique=True, nullable=False)
+    # user_password = db.Column(db.String(15), unique=True, nullable=False)
+    # user_chars = db.relationship("Character", backref="user", cascade="all, delete", lazy=True)
