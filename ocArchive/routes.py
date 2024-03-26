04c8ab8c548f7b63aaf94c06eb8f7a6ae3ca1f0f
cosmicCode42 <cosmicanswer@gmail.com>
2024-03-26 17:36:18 +0000

    Added create_character route, created add_genre page

--- ocArchive/routes.py
+++ ocArchive/routes.py
@@ -6,4 +6,40 @@ from ocArchive.models import User, Genre, Character
 @app.route("/")
 def home():
     # home page
-    return render_template("index.html")
+    genres = list(Genre.query.order_by(Genre.genre_name).all()) #puts all genres in a list
+    return render_template("index.html", genres=genres)
+
+
+@app.route("/genres")
+def genres():
+    # list of characters in each genre
+    return render_template("genres.html", genres=genres)
+    
+
+@app.route("/add_genre", methods=["GET", "POST"])
+def add_genre():
+    # page for adding genres directly
+    if request.method == "POST":
+        genre = Genre(genre_name=request.form.get("genre_name"))
+        db.session.add(genre)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_genre.html")
+    
+
+@app.route("/create_character", methods=["GET", "POST"])
+def create_character():
+    # add characters, including their genres
+    genres = list(Genre.query.order_by(Genre.genre_name).all())
+    if request.method == "POST":
+        character = Character(
+            char_name=request.form.get("character_name"),
+            char_blurb=request.form.get("character_blurb"),
+            char_descript=request.form.get("character_description"),
+            char_is_usable=request.form.get("character_is_usable"),
+            genre_id=request.form.get("genre_id")
+        )
+        db.session.add(character)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("create_character.html", genres=genres)
