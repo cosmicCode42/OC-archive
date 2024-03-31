cf7fa133aba511a2ebab3415019bb369552f9eca
cosmicCode42 <cosmicanswer@gmail.com>
2024-03-31 16:16:09 +0000

    Added JavaScript to display/hide New Genre option on character creation page

--- ocArchive/static/js/script.js
+++ ocArchive/static/js/script.js
@@ -10,4 +10,16 @@ document.addEventListener('DOMContentLoaded', function() {
     // collapsible initialisation
     let collapsibles = document.querySelectorAll('.collapsible');
     M.Collapsible.init(collapsibles);
+
+    // display/hide Add New Genre option in character creation page
+    const genreSelect = document.getElementById('genre_id');
+    const newGenreInput = document.getElementById('new_genre_input');
+
+    genreSelect.addEventListener('change', function() {
+        if (genreSelect.value === 'new_genre') {
+            newGenreInput.style.display = 'block';
+        } else {
+            newGenreInput.style.display = 'none';
+        }
+    });
   });
